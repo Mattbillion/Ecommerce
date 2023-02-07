e30cdacd07e56e7286dd1f0aaea7306535d59a78
Morning <morning@admins-iMac.local>
2023-02-07 12:29:23 +0800

    sercer

--- src/App.js
+++ src/App.js
@@ -4,35 +4,41 @@ import { Link, Route, Routes } from "react-router-dom";
 import Main from "./component/Main/Main";
 import Middle from "./component/Main/Middle";
 import Dashboard from "./component/Dashboard/Dashboard";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import Products from "./component/Dashboard/UI/Products";
+import { asideMenu } from "../src/component/Util/data";
+import Aside from "./component/Dashboard/AsideMenu/Aside";
 
 function App() {
   const [data, setData] = useState();
-  
+
   // ...destructure
   // const arr = [{key:value},{bkabla:blabal}]
   // arr2 = arr;
   //   const arr2 = [...arr + arr]
   //   arr = [{key:value},{bkabla:blabal}]
 
-
   useEffect(() => {
-    axios.get("http://localhost:2021/products")
-    .then((res) => setData(res.data))
-
-  }, [])
-  console.log(data)
+    try {
+      axios
+        .get("http://localhost:5000/products")
+        .then((res) => setData(res.data));
+    } catch (err) {
+      console.log(err.message);
+    }
+  }, []);
+  console.log(data);
 
   return (
     <div>
       {/* <NavBar /> */}
+      <Aside asideMenu={asideMenu} />
       <Routes>
         <Route path="/" element={<Main data={data} />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/books" />
         <Route path="/Middle" element={<Middle />} />
-        <Route path="/products" element={<Products data={data}/>} />
+        <Route path="/products" element={<Products data={data} />} />
       </Routes>
       {/* <Footer /> */}
     </div>
---
+++ src/component/Util/data.js
@@ -0,0 +1,21 @@
+export const asideMenu = [
+  {
+    nama: "Хянах самбар", url: "/"
+  },
+  {
+    nama: "Бүтээгдэхүүнүүд", url: "/products"
+  },
+  {
+    nama: "Захиалгууд", url: "/orders"
+  },
+  {
+    nama: "Хэрэглэгчид", url: "/users"
+  },
+  {
+    nama: "Модератор", url: "/moderator"
+  },
+  {
+    nama: "Тохиргоо", url: "settings"
+  },
+
+]
